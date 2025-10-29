9c935219c1938386a85bb79d1f512dc50c2edb06
VixenLaRuk <vixenlaruk@gmail.com>
2025-10-28 22:14:41 -0700

    finish

--- ComplexNumber/src/App.java
+++ ComplexNumber/src/App.java
@@ -1,4 +1,11 @@
  public class App {
+
+    /**
+     * The main method that creates ComplexNumber instances and preforms
+     * arithmetic operations, printing the results as an output.
+     * @param args Command-Line arguments (not used).
+     * @throws Exception If an error occurs during execution.
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         ComplexNumber number1 = new ComplexNumber(1, 2);
@@ -7,5 +14,7 @@
         System.out.println("second number: " + number2);
         System.out.println("Sum: " + number1.add(number2));
         System.out.println("Multiplication: " + number1.multiply(number2));
+        System.out.println("Subtraction: " + number1.subtract(number2));
+        System.out.println("Division: " + number1.divide(number2));
     }
 }
--- ComplexNumber/src/ComplexNumber.java
+++ ComplexNumber/src/ComplexNumber.java
@@ -1,32 +1,81 @@
+/**
+ * This program contructs a complex number calculator and runs using the main
+ * method in the App.java file.
+ * @author Vixen La Ruk
+ * @since 10/28/2025
+ */
 public class ComplexNumber {
 
-    private double real;
-    private double imag;
+    private double real; // The real part of the complex number
+    private double imag; // The imaginary part of the complex number
 
     ComplexNumber() { // default constructor
         real = 0.0;
         imag = 0.0;
     }
 
-    ComplexNumber(double real, double imag) { // overloaded constructor
+    /**
+     * Overloaded constructor that initializes the complex number with given
+     * real and imaginary parts.
+     * @param real The real part of the complex number.
+     * @param imag The imaginary part of the complex number.
+     */
+    ComplexNumber(double real, double imag) {
         this.real = real;
         this.imag = imag;
     }
 
+    /**
+     * Returns a string representation of the complex number in the format "a.b + c.di".
+     * @return A string representing the complex number.
+     */
     public String toString() {
         return String.format("%.1f + %.1fi", this.real, this.imag);
     }
 
+    /**
+     * Adds this complex number to another complex number.
+     * @param other The other complex number to add.
+     * @return A new ComplexNumber representing the sum.
+     */
     public ComplexNumber add(ComplexNumber other) {
 
         ComplexNumber newNumber = new ComplexNumber(this.real + other.real, this.imag + other.imag);
         return newNumber;
     }
 
+    /**
+     * Subtracts another complex number from this complex number.
+     * @param other The other complex number to subtract.
+     * @return A ComplexNumber representing the difference.
+     */
+    public ComplexNumber subtract(ComplexNumber other) {
+        ComplexNumber newNumber = new ComplexNumber(this.real - other.real, this.imag - other.imag);
+        return newNumber;
+    }
+
+    /**
+     * Multiplies this complex number by another complex number.
+     * @param other The other complex number to multiply by.
+     * @return A new ComplexNumber representing the product.
+     */
     public ComplexNumber multiply(ComplexNumber other) {
 
         double real = (this.real * other.real) + (this.imag * other.imag * -1);
         double imag = this.real * other.imag + this.imag * other.real;
+        ComplexNumber newNumber = new ComplexNumber(real, imag);
+        return newNumber;
+    }
+
+    /**
+     * Divides this complex number by another complex number.
+     * @param other The other complex number to divide by.
+     * @return A new ComplexNumber representing the quotient.
+     */
+    public ComplexNumber divide(ComplexNumber other) {
+        double denominator = other.real * other.real + other.imag * other.imag;
+        double real = (this.real * other.real + this.imag * other.imag) / denominator;
+        double imag = (this.imag * other.real - this.real * other.imag) / denominator;
         ComplexNumber newNumber = new ComplexNumber(real, imag);
         return newNumber;
     }
